be761648175d50ab6cc9a28ff8aebfa88b6df7c0
YarCode01 <yaraslau.ivashynka@studbocconi.it>
2024-05-05 23:57:52 +0200

    working Dataset class

--- dataset.py
+++ dataset.py
@@ -1,34 +1,55 @@
 
 import numpy as np
-from skimage import io
-from skimage.transform import resize
+from PIL import Image
 import matplotlib.pyplot as plt
 import random
 import matplotlib.patches as patches
-from utils import *
-from model import *
 import os
 
+from torchvision.io import read_image
 import torch
 import torchvision
+from torchvision import transforms
 from torchvision import ops
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 from torch.nn.utils.rnn import pad_sequence
 
-def parse(self):
+DEFAULT_IMG_PATH = "data/images"
+DEFAULT_ANNOTATION_PATH = "data/annotation_txts"
+
+def parse_path(img_path, annotation_path):
     # List all files in the given directory
-    file_names = os.listdir(self.img_path)
+    file_names = os.listdir(img_path)
     
     # Filter out non-image files and remove extensions, assuming images end with typical image file extensions
     image_extensions = {'.jpg', '.jpeg', '.png', '.bmp', '.tiff'}
-    path_images = [f"{self.img_path}/{file_name}" for file_name in file_names]
+    path_images = [f"{img_path}/{file_name}" for file_name in file_names]
     image_filenames_no_ext = [os.path.splitext(file)[0] for file in file_names if os.path.splitext(file)[1].lower() in image_extensions]
-    path_annotations = [f"{self.img_path}/{file_name}.txt" for file_name in image_filenames_no_ext]
+    assert len(image_filenames_no_ext) == len(path_images)
+    path_annotations = [f"{annotation_path}/{file_name}.txt" for file_name in image_filenames_no_ext]
     return path_images, path_annotations
 
-class DetectionDataset(Dataset):
+def parse_annotation(annotation_path):
+    boxes = []
+    labels = []
+    with open(annotation_path, 'r') as file:
+            for line in file:
+                data = line.strip().split()
+                class_label = int(data[0])
+                class_label = 0 # the class in the text file is wrong
+
+                # Convert coordinates to [x_min, y_min, x_max, y_max]
+                x_min = float(data[1])
+                y_min = float(data[2])
+                x_max = float(data[3])
+                y_max = float(data[4])
+                boxes.append([x_min, y_min, x_max, y_max])
+                labels.append(class_label)
+    return torch.as_tensor(boxes, dtype=torch.float32), torch.as_tensor(labels, dtype=torch.int64)
+
+class BoarDataset(Dataset):
     '''
     A Pytorch Dataset class to load the images and their corresponding annotations.
     
@@ -38,48 +59,38 @@ class DetectionDataset(Dataset):
     gt bboxes: torch.Tensor of size (B, max_objects, 4)
     gt classes: torch.Tensor of size (B, max_objects)
     '''
-    def __init__(self, img_path, annotation_path):
+    def __init__(self, img_path=DEFAULT_IMG_PATH, annotation_path=DEFAULT_ANNOTATION_PATH, transform=None):
         self.annotation_path = annotation_path
         self.img_path = img_path
-        
-        self.img_data_all, self.gt_bboxes_all, self.gt_classes_all = self.get_data()
+        self.transform = transform or transforms.Compose([
+            transforms.ToTensor(), 
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        self.imgs, self.boxes, self.labels = self.get_data()
         
     def __len__(self):
-        return self.img_data_all.size(dim=0)
+        return self.imgs.size(dim=0)
     
     def __getitem__(self, idx):
-        return self.img_data_all[idx], self.gt_bboxes_all[idx], self.gt_classes_all[idx]
+        return self.imgs[idx], self.boxes[idx], self.labels[idx]
         
     def get_data(self):
-        img_data_all = []
-        gt_idxs_all = []
+        imgs = []
+        classes = []
+        boxes = []
+        path_images, path_annotations = parse_path(img_path=self.img_path, annotation_path=self.annotation_path)
+        for img_path, annotation_path in zip(path_images, path_annotations):
+
+            img = Image.open(img_path).convert("RGB")
+            if self.transform:
+                img = self.transform(img)
+            boxes_tensor, labels_tensor = parse_annotation(annotation_path)
+            imgs.append(img)
+            boxes.append(boxes_tensor)
+            classes.append(labels_tensor)
         
-        file_names = parse(self.annotation_path, self.img_dir)
-        
-        for i, file_name in enumerate(file_names):
-            img_path
-            # skip if the image path is not valid
-            if not os.path.exists(img_path):
-                continue
-                
-            # read and resize image
-            img = io.imread(img_path)
-            
-            # convert image to torch tensor and reshape it so channels come first
-            img_tensor = torch.from_numpy(img).permute(2, 0, 1)
-            
-            # encode class names as integers
-            gt_classes = gt_classes_all[i]
-            gt_idx = torch.Tensor([self.name2idx[name] for name in gt_classes])
-            
-            img_data_all.append(img_tensor)
-            gt_idxs_all.append(gt_idx)
-        
-        # pad bounding boxes and classes so they are of the same size
-        gt_bboxes_pad = pad_sequence(gt_boxes_all, batch_first=True, padding_value=-1)
-        gt_classes_pad = pad_sequence(gt_idxs_all, batch_first=True, padding_value=-1)
-        
-        # stack all images
-        img_data_stacked = torch.stack(img_data_all, dim=0)
-        
-        return img_data_stacked.to(dtype=torch.float32), gt_bboxes_pad, gt_classes_pad
+        imgs = torch.stack(imgs, dim=0)
+        # boxes = pad_sequence(boxes, batch_first=True, padding_value=0)
+        # classes = pad_sequence(classes, batch_first=True, padding_value=0)  # Use -1 for classes padding if it is a safe 'ignore' index
+
+        return imgs, boxes, classes
